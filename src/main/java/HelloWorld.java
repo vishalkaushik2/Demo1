245ebc0b0d68114751bace79131147d334f9ee7b
vishalkaushik2 <vkaushik@DDN.UPES.AC.IN>
2017-08-09 13:10:04 +0530

    Second Change

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -3,5 +3,6 @@ class HelloWorld
 	public static void main(String a[])
 	{
 		System.out.print("Hello");
+		Syste,.out.print("Today is 9 Aug, 2017"); // Second Requirement Change as per client
 	}
 }
