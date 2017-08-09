a27f9abdcf20d6b5eedc44926fd9eb2ea9d148b3
vishalkaushik2 <30857331+vishalkaushik2@users.noreply.github.com>
2017-08-09 15:05:09 +0530

    Update HelloWorld.java
    Adding another class Second

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,11 +1,16 @@
 class HelloWorld
 {
+	class Second
+	{
+		System.out.println("I am Second");
+	}
 	public static void main(String a[])
 	{
 		System.out.print("Hello");
 		System.out.print("Today is 9 Aug, 2017"); // Second Requirement Change as per client
 		
-		
+		Second ob1;	
 		
 	}
+	
 }
